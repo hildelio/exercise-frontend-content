db4c1521e7f84a61cc377a9b4819be6cccaad2e4
hildelio <hildelio@gmail.com>
2022-10-19 18:11:49 -0300

    finalizando exercicio

--- src/Content.js
+++ src/Content.js
@@ -1,16 +1,40 @@
 import React, { Component } from 'react';
-import conteudos from './Data';
+
+const conteudos = [
+  {
+    nome: 'JavaScript assíncrono',
+    bloco: 9,
+    status: 'já aprendi',
+  },
+  {
+    nome: 'Composição de Componentes',
+    bloco: 10,
+    status: 'estou aprendendo',
+  },
+  {
+    nome: 'Composição de Estados',
+    bloco: 11,
+    status: 'aprenderei',
+  },
+  {
+    nome: 'Redux',
+    bloco: 15,
+    status: 'aprenderei',
+  },
+];
 
 class Content extends Component {
   render() {
     return (
-      <div>
+      <ul>
         {
-          conteudos.map((status, nome, bloco) => (
-            console.log(status, nome, bloco)
+          conteudos.map(({ nome, bloco, status }) => (
+            <li key={ nome }>
+              {`Eu ${status} o conteúdo ${nome} no bloco ${bloco} do conteúdo`}
+            </li>
           ))
         }
-      </div>
+      </ul>
     );
   }
 }
